ffa15860b4e50ef7937e7500f6d3cb6234f4d4da
Darius Bacon <darius@wry.me>
2012-11-29 16:28:12 -0500

    tighten up lexer spec

--- chatlang_lexer.py
+++ chatlang_lexer.py
@@ -4,37 +4,14 @@ RESERVED = 'RESERVED'
 INT      = 'INT'
 ID       = 'ID'
 
-token_regex_list = [
-    (r'[ \n\t]+', None),
-    (r'#[^\n]*', None),
-    (r'\:=', RESERVED),
-    (r'\(', RESERVED),
-    (r'\)', RESERVED),
-    (r';', RESERVED),
-    (r'\:', RESERVED),
-    (r'\+', RESERVED),
-    (r'-', RESERVED),
-    (r'\*', RESERVED),
-    (r'/', RESERVED),
-    (r'<=', RESERVED),
-    (r'<', RESERVED),
-    (r'>=', RESERVED),
-    (r'>', RESERVED),
-    (r'==', RESERVED),
-    (r'=', RESERVED),
-    (r'!=', RESERVED),
-    (r'and\b', RESERVED),
-    (r'or\b', RESERVED),
-    (r'not\b', RESERVED),
-    (r'if\b', RESERVED),
-    (r'then\b', RESERVED),
-    (r'else\b', RESERVED),
-    (r'while\b', RESERVED),
-    (r'do\b', RESERVED),
-    (r'end\b', RESERVED),
-    (r'[0-9]+', INT),
-    (r'[A-Za-z][A-Za-z0-9_]*', ID),
-]
+reserved = (r'\:= \( \) ; \: \+ - \* / <= < >= > == = != '
+            + r'and\b or\b not\b if\b then\b else\b while\b do\b end\b')
+
+token_regex_list = (  [(r'[ \n\t]+', None),
+                       (r'#[^\n]*', None)]
+                    + [(s, RESERVED) for s in reserved.split()]
+                    + [(r'[0-9]+', INT),
+                       (r'[A-Za-z][A-Za-z0-9_]*', ID)])
 
 def chatlang_lexer(character_stream):
     return gen_lexer.lexer(character_stream, token_regex_list)
