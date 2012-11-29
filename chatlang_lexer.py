487f1226d9a1ee0afd8a79e46693f83e7c6e16e5
Darius Bacon <darius@wry.me>
2012-11-29 16:29:19 -0500

    refactor regexes

--- chatlang_lexer.py
+++ chatlang_lexer.py
@@ -7,10 +7,10 @@ ID       = 'ID'
 reserved = (r'\:= \( \) ; \: \+ - \* / <= < >= > == = != '
             + r'and\b or\b not\b if\b then\b else\b while\b do\b end\b')
 
-token_regex_list = (  [(r'[ \n\t]+', None),
+token_regex_list = (  [(r'\s+', None),
                        (r'#[^\n]*', None)]
                     + [(s, RESERVED) for s in reserved.split()]
-                    + [(r'[0-9]+', INT),
+                    + [(r'\d+', INT),
                        (r'[A-Za-z][A-Za-z0-9_]*', ID)])
 
 def chatlang_lexer(character_stream):
