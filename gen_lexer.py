34ad4136492b47632b9fbc4c3c2cada3258ac1ea
Darius Bacon <darius@wry.me>
2012-11-29 15:34:16 -0500

    refactor lexer

--- gen_lexer.py
+++ gen_lexer.py
@@ -1,19 +1,16 @@
-import sys
 import re
 
-def lexer(character_stream, token_regex_list):
-    char_pos = 0
-    compiled_regexes = [(re.compile(p), tag) for p, tag in token_regex_list]
-    while char_pos < len(character_stream):
-        for token_regex in compiled_regexes:
-            regex, tag = token_regex
-            match = regex.match(character_stream, char_pos)
+def lexer(character_stream, token_specifiers):
+    choices = [(re.compile(regex), tag)
+               for regex, tag in token_specifiers]
+    pos = 0
+    while pos < len(character_stream):
+        for regex, tag in choices:
+            match = regex.match(character_stream, pos)
             if match:
-                matched_text = match.group(0)
-                if tag:
-                    yield (matched_text, tag)
+                if tag: yield match.group(0), tag
+                pos = match.end(0)
                 break
         else:
             raise Exception("Lexical error",
-                            character_stream[char_pos:].split()[0])
-        char_pos = match.end(0)
+                            character_stream[pos:].split()[0])
